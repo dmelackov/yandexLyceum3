702cb9221ad1bbec9949c9683eab38b916e46419
BuildTools <unconfigured@null.spigotmc.org>
2021-02-04 18:34:51 +0300

    Upload

--- main.py
+++ main.py
@@ -4,33 +4,34 @@ from io import BytesIO
 import requests
 
 from PIL import Image
-from utils import get_size, get_pos
+from utils import get_size, get_pos, get_response, get_org_pos, PointToPointCenter, VectorLenght
 
 if __name__ == '__main__':
-
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    search_api_server = "https://search-maps.yandex.ru/v1/"
     toponym_to_find = " ".join(sys.argv[1:])
 
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    json_response = get_response(geocoder_api_server, {"geocode": toponym_to_find,
+                                                       "format": "json",
+                                                       "apikey": "40d1649f-0493-4b70-98ba-98533de7710b"})
+    json_response2 = get_response(search_api_server,
+                                  {"text": "Аптека", "lang": "ru_RU",
+                                   "type": "biz", "ll": ",".join(get_pos(json_response)),
+                                   "apikey": "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"})
 
-    geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": toponym_to_find,
-        "format": "json"}
+    pos2 = get_org_pos(json_response2)
+    pos1 = get_pos(json_response)
+    size = get_size(json_response)
+    print(json_response2["features"][0]["properties"]["CompanyMetaData"]["address"])
+    print(json_response2["features"][0]["properties"]["CompanyMetaData"]["name"])
+    print(json_response2["features"][0]["properties"]["CompanyMetaData"]["Hours"]["text"])
+    print(VectorLenght(pos1, pos2) * 40000 / 360 * 1000, "м")
 
-    response = requests.get(geocoder_api_server, params=geocoder_params)
-
-    if not response:
-        exit(-1)
-        pass
-
-    json_response = response.json()
-
-    pos = get_pos(json_response)
     map_params = {
-        "ll": ",".join(pos),
-        "spn": ",".join(get_size(json_response)),
+        "ll": ",".join(PointToPointCenter(pos1, pos2)),
+        "spn": ",".join(size),
         "l": "map",
-        "pt": f"{pos[0]},{pos[1]},flag"
+        "pt": f"{','.join(pos2)},flag~{','.join(pos1)},flag"
     }
 
     map_api_server = "http://static-maps.yandex.ru/1.x/"
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import requests
+import math
+
+
 def get_size(json_response):
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
@@ -6,9 +10,33 @@ def get_size(json_response):
     longitude_size, lattitude_size = [xstart - xstop, ystart - ystop]
     return str(longitude_size), str(lattitude_size)
 
+
 def get_pos(json_response):
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
     toponym_coodrinates = toponym["Point"]["pos"]
     toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
     return toponym_longitude, toponym_lattitude
+
+
+def get_org_pos(json_response):
+    coords = json_response["features"][0]["geometry"]["coordinates"]
+    return str(coords[0]), str(coords[1])
+
+
+def get_response(api_server, params):
+    response = requests.get(api_server, params=params)
+    if not response:
+        print(response.content)
+        exit(-1)
+        pass
+    json_response = response.json()
+    return json_response
+
+
+def PointToPointCenter(a, b):
+    return str((float(a[0]) + float(b[0])) / 2), str((float(a[1]) + float(b[1])) / 2)
+
+
+def VectorLenght(a, b):
+    return math.sqrt((float(a[0]) - float(b[0])) ** 2 + (float(a[1]) - float(b[1])) ** 2)
